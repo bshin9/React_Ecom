c5f29ddd84f211294bc2bb45cb060b6b9f690941
Brian Shin <bshin9@yahoo.com>
2019-12-09 11:29:59 -0500

    changed

---
+++ client/build/precache-manifest.0083d43b9db8155eaf8cb992b13a46b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "466448417b2ea939e7d08ab2a20a3049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b452befcad0a9393fef",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ee895b7344bbbcb2f1d5",
+    "url": "/static/css/main.812fb025.chunk.css"
+  },
+  {
+    "revision": "4b452befcad0a9393fef",
+    "url": "/static/js/2.3e83b116.chunk.js"
+  },
+  {
+    "revision": "ee895b7344bbbcb2f1d5",
+    "url": "/static/js/main.d6982479.chunk.js"
+  },
+  {
+    "revision": "86b6e0e317031fc777a1",
+    "url": "/static/js/runtime-main.310d6c01.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,19 +1,30 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const PORT = 8000;
 const app = express();
-const morgan = require('morgan') 
-const helmet = require('helmet')
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 // Helmet will set various HTTP headers to help protect your app
 // Morgan logs through the terminal whenever an event is triggered
-app.use(helmet(), morgan('tiny'))
+app.use(helmet(), morgan("tiny"));
+
+const port = process.env.PORT || 8000;
+let uri = "";
+
+// Serve up static assets (heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  uri = process.env.ATLAS_URI; // connection string for Atlas here
+} else {
+  uri = process.env.ATLAS_URI; // connection string for localhost mongo here
+}
 
 const apiRoutes = require("./routes/apiRoutes");
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-})
+  console.log(`listening on port ${PORT}`);
+});
 
 module.exports = app;
